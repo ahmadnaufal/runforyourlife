92678b63741f21ea342f42dc8156498c2417b8c4
Ahmad N. F <fahmadnaufal@gmail.com>
2016-04-11 09:53:00 +0700

    added init player

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	//Material texture offset rate
+	float speed = .1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Offset the material texture at a constant rate
+		float offset = Time.time * speed;                             
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, -offset); 
+	}
+}
